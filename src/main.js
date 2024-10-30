893f63471aeea650e1d50c9fd9cdfb4f1a25837a
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2024-10-30 10:49:35 +0900

    [ optimize ] main.js Modalクラス 最適化

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 class Modal {
+	constructor() {
+		if(document.querySelector('.modals')) document.querySelector('.modals').remove();
+
+		document.querySelector('body').innerHTML += `<div class="modals"></div>`;
+	}
+
 	install(setData = {}) {
 		setData.id      = setData.id      ? setData.id      : 'modal';
 		setData.content = setData.content ? setData.content : '';
 
+		if(!document.querySelector('.modals')) return false;
 		if(document.querySelector('#' + setData.id)) return false;
 
-		document.querySelector('body').innerHTML += `<div class="modal" id="${setData.id}"><div class="container">${setData.content}</div></div>`;
+		document.querySelector('.modals').innerHTML += `<div class="modal" id="${setData.id}"><div class="container">${setData.content}</div></div>`;
 
 		return true;
 	}
@@ -23,9 +30,8 @@ class Modal {
 
 class App {
 	constructor() {
-		this.modal = new Modal();
-
 		window.addEventListener('load', () => {
+			this.modal = new Modal();
 			this.initialize();
 		});
 	}
@@ -37,7 +43,7 @@ class App {
 
 		document.querySelector('body').innerHTML += `<header></header><main><div class="container"><div class="cards"></div></div></main><footer><div class="container"><p>&copy; 2023&nbsp;<a href="//github.com/kanaaa224/" style="color:inherit;"><u>kanaaa224</u></a>.</p></div></footer>`;
 
-		this.modal.install({ 'content': '<h1>開発中！</h1><p>coming soon!</p><div class="btn"><div onclick=app.modal.uninstall();>閉じる</div></div>' });
+		this.modal.install({ 'content': '<h1>開発中！</h1><p>coming soon!</p><div class="button"><div onclick=app.modal.uninstall();>閉じる</div></div>' });
 	}
 }
 
